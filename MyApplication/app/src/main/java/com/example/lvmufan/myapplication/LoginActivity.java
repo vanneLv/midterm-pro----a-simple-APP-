2172772a5977e09081fded7662e6c655d609cc20
Dossy1234 <44916107+Dossy1234@users.noreply.github.com>
2018-11-24 16:31:29 +0800

    okhttp,但是json的部分都是报错

--- MyApplication/app/src/main/java/com/example/lvmufan/myapplication/LoginActivity.java
+++ MyApplication/app/src/main/java/com/example/lvmufan/myapplication/LoginActivity.java
@@ -5,17 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.DefaultRetryPolicy;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -35,7 +35,7 @@ public class LoginActivity extends AppCompatActivity {
         ClickListener cl = new ClickListener();
 
         Button loginButton = (Button) this.findViewById(R.id.sign_in_button);
-        loginButton.setOnClickListener(cl);
+        loginButton.setOnClickListener(cl); //等待button被按的响应（不知道是否必要，但是网上有人写了）
 
         Button registerButton = (Button) this.findViewById(R.id.sign_up_button);
         registerButton.setOnClickListener(cl);
@@ -47,11 +47,11 @@ public class LoginActivity extends AppCompatActivity {
         public void onClick(View v) {
             switch (v.getId()) {
                 case R.id.sign_in_button:
-                    processLogin();
+                    processLogin();//跳转到登录进程
                     break;
                 case R.id.sign_up_button:
                     Intent intent = new Intent(LoginActivity.this,SignupActivity.class);
-                    startActivity(intent);
+                    startActivity(intent);//跳转到注册界面
                     break;
                 default:
                     break;
@@ -63,60 +63,102 @@ public class LoginActivity extends AppCompatActivity {
     public void processLogin() {
         EditText usernameText = (EditText) this.findViewById(R.id.login_username);
         EditText passwordText = (EditText) this.findViewById(R.id.login_password);
-
         String username = usernameText.getText().toString();
         String password = passwordText.getText().toString();
 
-        HashMap<String,String> params = new HashMap<String,String>();
-        params.put("name", username);
-        params.put("password", password);
+        OkHttpClient login_client = new OkHttpClient();//用okhttp的网络架构进行登录
+        RequestBody postBody = new FormBody.Builder()//用formbody的形式向服务器传输用户名和密码
+                .add("username", username)
+                .add("password", password)
+                .build() ;
 
-        String url = "10.15.82.223:9090/app_get_data/app_signincheck ";
-        this.run(params,url);
+        Request request = new Request.Builder()
+                .url("http://10.15.82.223:9090/app_get_data/app_signincheck")//指定访问的服务器地址
+                .post(postBody)
+                .build() ;
+
+        Response response = login_client.newCall(request).execute();
+        String responseData = response.body().string();
+        parseJSONWithJSONObject(responseData);//调用parseJSONWithJSONObject()方法来解析数据
+    }catch (Exception e){
+        e.printStackTrace();
     }
+}
 
-    //上传数据到服务器匹配
-    public void run(HashMap<String, String> params, String url) {
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-
-        JsonObjectRequest jsonRequest = new JsonObjectRequest(
-                Request.Method.POST, url,
-                new JSONObject(params), new Response.Listener<JSONObject>() {
-            public void onResponse(JSONObject response) {
-                String status = null;
-                try {
-                    status = response.getString("status");
-                    Log.e("status","status"+status);
-                    status = "0";
-                    Log.e("status","status"+status);
-                    if (status.equals("0")) {
-                        Toast.makeText(LoginActivity.this, "登录成功!",Toast.LENGTH_LONG ).show();
-                        Intent intent = new Intent(LoginActivity.this,MainActivity.class);
-                        startActivity(intent);
-                    } else {
-                        Toast.makeText(LoginActivity.this, "登录失败！",Toast.LENGTH_LONG ).show();
+    private void parseJSONWithJSONObject(String jsonData){
+        try{
+            JSONArray jsonArray = new JSONArray(jsonData);
+            for(int i = 0;i < jsonArray.length();i++){
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                String message = jsonObject.getString("msg");
+                String msg = unicodeToUtf8(message);//对数据进行Unicode转码为中文字符
+                Log.d("msg",msg);//打印传输回来的消息
+            }
+        }
+    }
+    public static String unicodeToUtf8(String theString) {
+        char aChar;
+        int len = theString.length();
+        StringBuffer outBuffer = new StringBuffer(len);
+        for (int x = 0; x < len;) {
+            aChar = theString.charAt(x++);
+            if (aChar == '\\') {
+                aChar = theString.charAt(x++);
+                if (aChar == 'u') {
+                    // Read the xxxx
+                    int value = 0;
+                    for (int i = 0; i < 4; i++) {
+                        aChar = theString.charAt(x++);
+                        switch (aChar) {
+                            case '0':
+                            case '1':
+                            case '2':
+                            case '3':
+                            case '4':
+                            case '5':
+                            case '6':
+                            case '7':
+                            case '8':
+                            case '9':
+                                value = (value << 4) + aChar - '0';
+                                break;
+                            case 'a':
+                            case 'b':
+                            case 'c':
+                            case 'd':
+                            case 'e':
+                            case 'f':
+                                value = (value << 4) + 10 + aChar - 'a';
+                                break;
+                            case 'A':
+                            case 'B':
+                            case 'C':
+                            case 'D':
+                            case 'E':
+                            case 'F':
+                                value = (value << 4) + 10 + aChar - 'A';
+                                break;
+                            default:
+                                throw new IllegalArgumentException(
+                                        "Malformed   \\uxxxx   encoding.");
+                        }
                     }
-                } catch (JSONException e) {
-                    e.printStackTrace();
+                    outBuffer.append((char) value);
+                } else {
+                    if (aChar == 't')
+                        aChar = '\t';
+                    else if (aChar == 'r')
+                        aChar = '\r';
+                    else if (aChar == 'n')
+                        aChar = '\n';
+                    else if (aChar == 'f')
+                        aChar = '\f';
+                    outBuffer.append(aChar);
                 }
-            }
-        }, new Response.ErrorListener() {
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        }){
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                HashMap<String, String> headers = new HashMap<String, String>();
-                headers.put("Accept", "application/json");
-                headers.put("Content-Type", "application/json; charset=UTF-8");
-                return headers;
-            }
-        };
-        jsonRequest.setRetryPolicy(new DefaultRetryPolicy(10 * 1000, 2, 1.0f));
-        requestQueue.add(jsonRequest);
-        requestQueue.start();
+            } else
+                outBuffer.append(aChar);
+        }
+        return outBuffer.toString();
     }
-
 }
 
